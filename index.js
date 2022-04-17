95001572801df2e706ba98b8b5b60cab48d27210
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-04-18 00:55:28 +0100

    fixed a line in vloume side not changing values

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let massLine = document.getElementById("mass")
 // formatiing them
 function changeValue() {
     lengthLine.textContent = `${main.value} meters = ${main.value * 3.28} feet | ${main.value} feet = ${main.value * 0.30} meters`
-    volumeLine.textContent = `${main.value} liters = ${main.value * 0.26} gallons | 20 gallons = ${main.value * 3.78} liters`
+    volumeLine.textContent = `${main.value} liters = ${main.value * 0.26} gallons | ${main.value} gallons = ${main.value * 3.78} liters`
     massLine.textContent = `${main.value} kilos = ${main.value * 2.20} pounds | ${main.value} pounds = ${main.value * 0.45} kilos`
 
 }
